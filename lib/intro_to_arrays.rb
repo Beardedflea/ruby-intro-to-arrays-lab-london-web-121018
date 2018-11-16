8b1f11a6ed47327fe648fe16bfea044b9cdbbe26
Linzell Godfrey <linzell.godfrey@live.co.uk>
2018-11-16 20:18:36 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,24 @@
+def instantiate_new_array
+  return empty_array =[]
+end
+
+def array_with_two_elements
+  return array_du_deux = ["un","deux"]
+end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+
+def first_element_with_array_methods(array)
+  temp_array = array.pop
+  return temp_array
+end
